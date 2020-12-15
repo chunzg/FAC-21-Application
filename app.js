108b25d2b073885527cd51a9889baa907f50498a
chunzg <chunzhang53@gmail.com>
2020-12-15 14:41:17 +0000

    Add functionality to the menu

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const bgBtn = document.querySelector('#bg');
 const groupOne = document.querySelectorAll('.original');
 const groupTwo = document.querySelectorAll('.original-two');
 
+//MENU 
+const mainNav = document.querySelector('.main-nav');
+const navBtn = document.querySelector('.navbar-plus');
+const navLinks = document.querySelectorAll('.nav-link');
+
 //Invert background colours
 bgBtn.addEventListener('click', invertColours);
 
@@ -16,3 +21,24 @@ function invertColours() {
 		x.style.transition = '.5s ease-in';
 	})
 }
+
+//Show/hide menu
+function navBtnRotate() {
+	navBtn.addEventListener('click', (e) => {
+		if(mainNav.classList.toggle('active')){
+			e.target.style.transform = 'rotate(45deg)';
+		} else if(mainNav.classList.toggle != 'active') {
+			e.target.style.transform = 'rotate(0deg)';
+		}
+	});
+}
+navBtnRotate()
+
+//Hide menu when a link is clicked
+navLinks.forEach((x) => {
+	x.addEventListener('click', () => {
+		mainNav.classList.toggle('active');
+		navBtn.style.transform = 'rotate(0deg)';
+	})
+})
+
