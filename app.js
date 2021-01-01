9525246e7be5452a4ad17f617125876729822688
chunzg <chunzhang53@gmail.com>
2021-01-01 17:15:03 +0000

    Try to fix carousel onload issue

--- app.js
+++ app.js
@@ -81,7 +81,9 @@ const width = slide[index].clientWidth;
 slideGroup.style.transform = `translateX(${-width * index}px)`; //this line sets the first image we see as index 3
 
 // Start automatic loop on page load
-let intervalId = setInterval(nextSlide, 2000); 
+let intervalId
+const startSlide = () => intervalId = setInterval(nextSlide, 2000); 
+startSlide();
 
 // When click the arrows...
 // Move to next slide
