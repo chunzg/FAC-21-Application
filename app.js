fefb722cfa6f774d3492631d0a471ce6c927c1b3
chunzg <chunzhang53@gmail.com>
2021-01-05 12:52:27 +0000

    Revert back to latest carousel code

--- app.js
+++ app.js
@@ -47,140 +47,110 @@ document.addEventListener('DOMContentLoaded', function() {
 	})
 
 	//--------------
-// CAROUSEL VARIABLES
-const slideContainer = document.querySelector('.carousel');
-const slideGroup = document.querySelector('.slides');
-const nextBtn = document.querySelector('.next-btn');
-const prevBtn = document.querySelector('.prev-btn');
-const interval = 3250;
-const playPause = document.querySelector('.pause');
-let slides = document.querySelectorAll('.slide');
-let index = 1; //bc 0 is clone of last slide
-let slideId; // this is later set to be the intervalID parameter. 
-let playing;
-
-// CLONE FIRST AND LAST CAROUSEL IMAGE
-const firstClone = slides[0].cloneNode(true);
-const lastClone = slides[slides.length - 1].cloneNode(true);
-
-// SET IDS FOR THESE TWO CLONES
-firstClone.id = 'first-clone'; 
-lastClone.id = 'last-clone';
-
-// PLACE CLONES AT START AND END OF SLIDES GROUP
-slideGroup.append(firstClone);
-slideGroup.prepend(lastClone);
-
-// SET SLIDE WIDTH
-const slideWidth = slides[index].clientWidth;
-
-// SET THE DISTANCE OF THE IMAGE MOVING HORIZONTALLY
-slideGroup.style.transform = `translateX(${-slideWidth * index}px)`;
-
-// FUNCTION TO START SLIDESHOW
-const startSlide = () => {
-	playing = true;
-	return slideId = setInterval(() => { // WHY DO I HAVE TO RETURN IT FOR IT TO WORK? //is this example of a closure?
-		moveToNextSlide();
-	}, interval);
-};
-
-// GET ALL OF THE SLIDES
-const getSlides = () => document.querySelectorAll('.slide');
-
-// WHEN THE CSS TRANSITION ENDS, KEEP GOING
-slideGroup.addEventListener('transitionend', () => {
-	slides = getSlides();
-	if (slides[index].id === firstClone.id) {
-		slideGroup.style.transition = 'none';
-		index = 1;
-		slideGroup.style.transform = `translateX(${-slideWidth * index}px)`;
+	const container = document.querySelector('.carousel');
+	const carousel = document.querySelector('.slides');
+	const nextBtn = document.querySelector('.next-btn');
+	const prevBtn = document.querySelector('.prev-btn');
+	const playPause = document.querySelector('.pause');
+	let slides = document.querySelectorAll('.slide');
+	let index = 1; //bc 0 is clone of last slide
+	// let intervalId; 
+	
+	// Clone first and last image
+	const firstClone = slides[0].cloneNode(true);
+	const lastClone = slides[slides.length - 1].cloneNode(true);
+	
+	// Set ids for new clones
+	firstClone.id = 'first-clone'; 
+	lastClone.id = 'last-clone';
+	
+	// Place clones at start and end of slide group
+	carousel.append(firstClone);
+	carousel.prepend(lastClone);
+	
+	// Set slide width and distance to move
+	const slideWidth = slides[index].clientWidth;
+	carousel.style.transform = `translateX(${-slideWidth * index}px)`;
+	
+	// Function to start moving
+	const startSlide = () => {
+		return intervalId = setInterval(nextSlide, 2000);
+	};
+	
+	// Redefine slides var bc length has changed
+	slides = document.querySelectorAll('.slide');
+	
+	// When css transition ends, keep going
+	carousel.addEventListener('transitionend', () => {
+		if(slides[index].id === firstClone.id) {
+			carousel.style.transition = 'none';
+			index = 1;
+			carousel.style.transform = `translateX(${-slideWidth * index}px)`;
+		}
+		if(slides[index].id === lastClone.id) {
+			carousel.style.transition = 'none';
+			index = slides.length - 2;
+			carousel.style.transform = `translateX(${-slideWidth * index}px)`;
+		}
+	});
+	
+	// Functions to move to next / prev slide
+	const nextSlide = () => {
+		if(index >= slides.length - 1) return;
+		index++;
+		carousel.style.transform = `translateX(${-slideWidth * index}px)`;
+		carousel.style.transition = '.8s';
 	}
-	if (slides[index].id === lastClone.id) {
-		slideGroup.style.transition = 'none';
-		index = slides.length - 2;
-		slideGroup.style.transform = `translateX(${-slideWidth * index}px)`;
+	const prevSlide = () => {
+		if(index <= 0) return;
+		index--;
+		carousel.style.transform = `translateX(${-slideWidth * index}px)`;
+		carousel.style.transition = '.7s';
 	}
-});
-
-// FUNCTION FOR MOVING TO NEXT SLIDE
-const moveToNextSlide = () => {
-	slides = getSlides();
-	if (index >= slides.length - 1) return;
-	index++;
-	slideGroup.style.transform = `translateX(${-slideWidth * index}px)`;
-	slideGroup.style.transition = '.8s';
-}
-
-// FUNCTION FOR MOVING TO PREVIOUS SLIDE
-const moveToPrevSlide = () => {
-	slides = getSlides();
-	if (index <= 0) return;
-	index--;
-	slideGroup.style.transform = `translateX(${-slideWidth * index}px)`;
-	slideGroup.style.transition = '.7s';
-}
-
-// WHEN CLICK THE ARROWS, MOVE TO NEXT OR PREVIOUS SLIDES
-nextBtn.addEventListener('click', moveToNextSlide);
-prevBtn.addEventListener('click', moveToPrevSlide);
-
-// START AUTOMATIC SLIDESHOW 
-startSlide();
-
-// PLAY PAUSE BUTTON - slideshow start/stop
-playPause.addEventListener('click', () => {
-	if(!slideId) {
-		slideId = startSlide(); // WHY DO I HAVE TO SPECIFY SLIDE ID = STARTSLIDE. WHY CAN'T IT JUST BE STARTSLIDE(). 
-		console.log('started');
-	} else {
-		clearInterval(slideId);
-		slideId = null;
-		console.log('stopped');
+	nextBtn.addEventListener('click', nextSlide);
+	prevBtn.addEventListener('click', prevSlide);
+	
+	//Start on load (here bc hoisting)
+	startSlide();
+	
+	// Start/stop function
+	function playOrPause() {
+		if(!intervalId) {
+			intervalId = setInterval(nextSlide, 2000); 
+			playPause.src = 'img/pause.png';
+		} else {
+			clearInterval(intervalId);
+			intervalId = null;
+			playPause.src = 'img/play.png';
+		}
 	}
-});
-
-// PLAY PAUSE BUTTON - image change
-playPause.addEventListener('click', function () { //why function not arrow works here???
-	var button = this; 
-	if(button.className != 'pause') {
-		button.src = 'img/pause.png';
-		button.className = 'pause';
-	} else if (button.className == 'pause') {
-		button.src = 'img/play.png';
-		button.className = 'play';
+	playPause.addEventListener('click', playOrPause);
+	
+	// Play pause mouseover event
+	container.addEventListener('mouseout', () => {
+		playPause.style.opacity = 0;
+		playPause.style.transition = '.5s';
+	})
+	container.addEventListener('mouseover', () => {
+		playPause.style.opacity = 1;
+		playPause.style.transition = '.5s';
+	})
+	
+	// Keyboard function
+	function onKeydown(e) {
+		switch(e.keyCode) {
+			case 37:
+				prevSlide();
+				break;
+			case 39:
+				nextSlide();
+				break;
+			case 32:
+				e.preventDefault();
+				playOrPause();
+		}
 	}
-	return false;
-});
-
-// PLAY PAUSE BUTTON - mouseover event
-slideContainer.addEventListener('mouseout', () => {
-	playPause.style.display = 'none';
-})
-slideContainer.addEventListener('mouseover', () => {
-	playPause.style.display = 'block';
-})
-
-// KEYBOARD FUNCTION
-document.onkeydown = function (event) {
-	event = event || window.event;
-	switch (event.keyCode) {
-		case 37:
-			leftArrowPressed();
-			break;
-		case 39:
-			rightArrowPressed();
-			break;
-	}
-}
-
-function leftArrowPressed() {
-	moveToPrevSlide();
-}
-function rightArrowPressed() {
-	moveToNextSlide();
-}
-
+	document.addEventListener('keydown', onKeydown);
 
 	//-------------
 	//Move text along the svg path according to the user's scroll position
